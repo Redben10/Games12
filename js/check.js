7264fd24bde537ed7a963a915556d9e5a47ff9fa
Redben10 <92760191+Redben10@users.noreply.github.com>
2024-12-06 19:40:40 +0000

    Update check.js

--- js/check.js
+++ js/check.js
@@ -1,6 +1,9 @@
+// Check if the user has access
 if (!localStorage.getItem('access')) {
-    // If they don't have access, redirect to "./"
-    window.location.href = './';
+    // If not, redirect to the password page
+    window.location.href = 'password.html';
 } else {
+    // If they have access, you can proceed with the main page content
     console.log('Access granted. Welcome to the main page!');
+    // Add your main page logic here
 }
